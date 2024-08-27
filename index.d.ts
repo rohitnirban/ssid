f32c4096e908d34e7e6270cee8dc7a88042b240a
Rohit Yadav <developer.royad@gmail.com>
2024-08-27 15:27:19 +0530

    added types to withSymbol

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,10 @@
 declare module 'ssid' {
-    export default function generateShortId(length?: number): string;
+    /**
+     * Generates a short ID with the specified length and symbol inclusion.
+     * 
+     * @param length - The length of the generated short ID. Default is 8 characters. Minimum length is 4 and maximum length is 10,000.
+     * @param withSymbols - A boolean indicating whether to include symbols. Default is true.
+     * @returns A string representing the generated short ID.
+     */
+    export default function generateShortId(length?: number, withSymbols?: boolean): string;
 }
