3a5a28bb96058fe3432337f905c1a9763079f156
Rohit Yadav <developer.royad@gmail.com>
2024-08-27 15:29:30 +0530

    fix some typo

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ declare module 'ssid' {
      * Generates a short ID with the specified length and symbol inclusion.
      * 
      * @param length - The length of the generated short ID. Default is 8 characters. Minimum length is 4 and maximum length is 10,000.
-     * @param withSymbols - A boolean indicating whether to include symbols. Default is true.
+     * @param withSymbols - A boolean indicating whether to include symbols. By Default is true.
      * @returns A string representing the generated short ID.
      */
     export default function generateShortId(length?: number, withSymbols?: boolean): string;
